951311b654e5888b316a8db1f3e64ea441c51c14
Alessandro <alessandromaria.calista@gmail.com>
2022-08-25 01:04:40 +0200

    Test commit.

--- src/main/java/tech/calista/traveler/Traveler.java
+++ src/main/java/tech/calista/traveler/Traveler.java
@@ -6,7 +6,7 @@ public final class Traveler extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getLogger().info("Testing GitHub Bot!");
 
     }
 
